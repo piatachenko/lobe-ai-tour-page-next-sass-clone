8d9a4609170f338d0c324a131be637544b20ed87
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-03-29 01:39:11 +0200

    video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,14 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import styles from "@/styles/Home.module.scss";
 import Head from "next/head";
+import { useState } from "react";
 
 export default function Home() {
+  const [video, setVideo] = useState(false);
+
+  function handleClick() {
+    setVideo(!video);
+  }
 
   return (
     <>
@@ -31,13 +37,52 @@ export default function Home() {
                 src="https://www.lobe.ai/assets/tour/Video.jpg"
                 alt="Learn more about Lobe by watching our tour video."
                 className={styles.previewVideoImage}
+                onClick={handleClick}
               />
+            </div>
+            <div className={video ? styles.introductionVideoContainer : styles.hidden}>
+              <button onClick={handleClick}>
+                <svg
+                  width="32px"
+                  height="32px"
+                  viewBox="0 0 32 32"
+                  version="1.1"
+                >
+                  <g
+                    stroke-width="1"
+                    fill="none"
+                    fill-rule="evenodd"
+                    opacity="0.699999988"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <g
+                      id="Button-/-Close"
+                      transform="translate(5.000000, 5.000000)"
+                      stroke-width="2"
+                    >
+                      <g transform="translate(-5.000000, -5.000000)">
+                        <line x1="27" y1="5" x2="5" y2="27"></line>
+                        <line x1="5" y1="5" x2="27" y2="27"></line>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              </button>
+              <iframe
+                width="1120"
+                height="699"
+                src="https://www.youtube.com/embed/Mdcw3Sb98DA"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
             </div>
             <div className={styles.downloadSection}>
               <p>Train your app with Lobe</p>
               <button>Download</button>
             </div>
-            
           </section>
         </main>
         <Footer />
