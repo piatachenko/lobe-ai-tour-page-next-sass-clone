b1f7a0cee7cd46b2752478396e6384cd262e9140
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-03-31 20:56:55 +0200

    fixed overlap on hover

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,50 +36,50 @@ export default function Home() {
               <img
                 src="https://www.lobe.ai/assets/tour/Video.jpg"
                 alt="Learn more about Lobe by watching our tour video."
-                className={styles.previewVideoImage}
+                className={styles.tourVideoPreviewImage}
                 onClick={handleClick}
               />
             </div>
-            {tourVideo ? (<div
-              className={styles.tourVideoContainer}
-            >
-              <button onClick={handleClick}>
-                <svg
-                  width="32px"
-                  height="32px"
-                  viewBox="0 0 32 32"
-                  version="1.1"
-                >
-                  <g
-                    stroke-width="1"
-                    fill="none"
-                    fill-rule="evenodd"
-                    opacity="0.699999988"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+            {tourVideo ? (
+              <div className={styles.tourVideoContainer}>
+                <button onClick={handleClick}>
+                  <svg
+                    width="32px"
+                    height="32px"
+                    viewBox="0 0 32 32"
+                    version="1.1"
                   >
                     <g
-                      id="Button-/-Close"
-                      transform="translate(5.000000, 5.000000)"
-                      stroke-width="2"
+                      stroke-width="1"
+                      fill="none"
+                      fill-rule="evenodd"
+                      opacity="0.699999988"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
                     >
-                      <g transform="translate(-5.000000, -5.000000)">
-                        <line x1="27" y1="5" x2="5" y2="27"></line>
-                        <line x1="5" y1="5" x2="27" y2="27"></line>
+                      <g
+                        id="Button-/-Close"
+                        transform="translate(5.000000, 5.000000)"
+                        stroke-width="2"
+                      >
+                        <g transform="translate(-5.000000, -5.000000)">
+                          <line x1="27" y1="5" x2="5" y2="27"></line>
+                          <line x1="5" y1="5" x2="27" y2="27"></line>
+                        </g>
                       </g>
                     </g>
-                  </g>
-                </svg>
-              </button>
-              <iframe
-                width="1120"
-                height="699"
-                src="https://www.youtube.com/embed/Mdcw3Sb98DA?autoplay=1&mute=1"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              ></iframe>
-            </div>) : null}
+                  </svg>
+                </button>
+                <iframe
+                  width="1120"
+                  height="699"
+                  src="https://www.youtube.com/embed/Mdcw3Sb98DA?autoplay=1&mute=1"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            ) : null}
             <div className={styles.downloadSection}>
               <p>Train your app with Lobe</p>
               <button>Download</button>
